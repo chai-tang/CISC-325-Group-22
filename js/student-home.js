797912948ee477c3f9922cc759b855edfa3adeef
chai-tang <chaitang.joshua@gmail.com>
2021-04-02 18:39:25 -0400

    Added filter buttons to homepage

--- js/student-home.js
+++ js/student-home.js
@@ -6,7 +6,9 @@ window.addEventListener("load",function(){
     // to unique pages. In practice, there would also be a function here to fetch all the
     // user's projects from a database and fill the table with that data. But this is just a 
     // prototype, so that's not my problem right now.
-    makeRowsClickable()
+    makeRowsClickable();
+
+    makeFilterButtonsWork();
     
 })
 
@@ -30,4 +32,53 @@ function makeRowsClickable(){
 */
 function redirectToProject(){
     window.location.href = "student-project.html";
+}
+
+// Makes the filter buttons work, ie they filter out the non matching project rows
+function makeFilterButtonsWork(){
+
+    // apply to the all-button
+    const allbutton = document.getElementById("all-button");
+    allbutton.addEventListener("click",function(){
+        showRows("all")
+    });
+
+    // apply to the math-button
+    const mathbutton = document.getElementById("math-button");
+    mathbutton.addEventListener("click",function(){
+        showRows("math")
+    });
+
+    // apply to the art-button
+    const artbutton = document.getElementById("art-button");
+    artbutton.addEventListener("click",function(){
+        showRows("art")
+    });
+
+    // apply to the science-button
+    const sciencebutton = document.getElementById("science-button");
+    sciencebutton.addEventListener("click",function(){
+        showRows("science")
+    });
+
+}
+
+
+// Makes all project rows with classname visible, and hides the others
+function showRows(classname){
+
+    const tbody = document.getElementsByTagName("tbody")[0];
+
+    // if classname is "all" then make everything visible
+    let isAll = classname == "all";
+
+    for (i=0; i<tbody.children.length; i++){
+        if (tbody.children[i].getAttribute("class") == classname || isAll){
+            tbody.children[i].style.display = "table-row";
+        }
+        else {
+            tbody.children[i].style.display = "none";
+        }
+    }
+
 }
